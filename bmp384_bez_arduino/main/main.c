92a1674412a815ec1dada95e978d968993be4bd1
Patryk Niczke <p.niczke@satrev.space>
2025-02-19 23:00:37 +0100

    Add calibration of the pressure

--- bmp384_bez_arduino/main/main.c
+++ bmp384_bez_arduino/main/main.c
@@ -16,10 +16,31 @@
 #define CONFIG_REG 0x1F
 #define ODR_REG 0x1D
 
+
+#define NVM_PAR_T1 0x31
+#define NVM_PAR_T2 0x33
+#define NVM_PAR_T3 0x35
+#define NVM_PAR_P1 0x36
+#define NVM_PAR_P2 0x38
+#define NVM_PAR_P3 0x3A
+#define NVM_PAR_P4 0x3B
+#define NVM_PAR_P5 0x3C
+#define NVM_PAR_P6 0x3E
+#define NVM_PAR_P7 0x40
+#define NVM_PAR_P8 0x41
+#define NVM_PAR_P9 0x42
+#define NVM_PAR_P10 0x44
+#define NVM_PAR_P11 0x45
+
 static const char *TAG = "SPI_Transaction";
 
 spi_device_handle_t spi;
 
+typedef struct {
+    float par_t1, par_t2, par_t3;
+    float par_p1, par_p2, par_p3, par_p4, par_p5, par_p6, par_p7, par_p8, par_p9, par_p10, par_p11;
+    float t_lin;
+} bmp384_calib_data_t;
 
 // Wpisywanie bajtu do rejestru
 esp_err_t bmp384_write_register(uint8_t reg, uint8_t value) {
@@ -133,6 +154,78 @@ esp_err_t bmp384_read_register_many_bytes(uint8_t reg, uint8_t* data, size_t len
 
 }
 
+void bmp384_read_calibration(bmp384_calib_data_t *calib, uint32_t uncomp_temp) {
+    uint8_t data[2];
+
+    bmp384_read_register_many_bytes(NVM_PAR_T1, data, 2);
+    calib->par_t1 = (uint16_t)((data[1] << 8) | data[0]) * 256.0;
+    
+    bmp384_read_register_many_bytes(NVM_PAR_T2, data, 2);
+    calib->par_t2 = (uint16_t)((data[1] << 8) | data[0]) / 1073741824.0;
+    
+    bmp384_read_register_many_bytes(NVM_PAR_T3, data, 1);
+    calib->par_t3 = (int8_t)data[0] / ((float)(1ULL << 48));
+    
+    bmp384_read_register_many_bytes(NVM_PAR_P1, data, 2);
+    calib->par_p1 = ((int16_t)((data[1] << 8) | data[0]) - 16384) / 1048576.0;
+    
+    bmp384_read_register_many_bytes(NVM_PAR_P2, data, 2);
+    calib->par_p2 = ((int16_t)((data[1] << 8) | data[0]) - 16384) / 536870912.0;
+    
+    bmp384_read_register_many_bytes(NVM_PAR_P3, data, 1);
+    calib->par_p3 = (int8_t)data[0] / 4294967296.0;
+    
+    bmp384_read_register_many_bytes(NVM_PAR_P4, data, 1);
+    calib->par_p4 = (int8_t)data[0] / 137438953472.0;
+    
+    bmp384_read_register_many_bytes(NVM_PAR_P5, data, 2);
+    calib->par_p5 = ((uint16_t)((data[1] << 8) | data[0])) * 8.0;
+    
+    bmp384_read_register_many_bytes(NVM_PAR_P6, data, 2);
+    calib->par_p6 = ((uint16_t)((data[1] << 8) | data[0])) / 64.0;
+    
+    bmp384_read_register_many_bytes(NVM_PAR_P7, data, 1);
+    calib->par_p7 = (int8_t)data[0] / 256.0;
+    
+    bmp384_read_register_many_bytes(NVM_PAR_P8, data, 1);
+    calib->par_p8 = (int8_t)data[0] / 32768.0;
+    
+    bmp384_read_register_many_bytes(NVM_PAR_P9, data, 2);
+    calib->par_p9 = ((int16_t)((data[1] << 8) | data[0])) / ((float)(1ULL << 48));
+    
+    bmp384_read_register_many_bytes(NVM_PAR_P10, data, 1);
+    calib->par_p10 = (int8_t)data[0] / ((float)(1ULL << 48));
+    
+    bmp384_read_register_many_bytes(NVM_PAR_P11, data, 1);
+    calib->par_p11 = (int8_t)data[0] / ((float)(1ULL << 48));
+
+    float partial_data1;
+    float partial_data2;
+
+    partial_data1 = (float)(uncomp_temp - calib->par_t1);
+    partial_data2 = (float)(partial_data1 * calib->par_t2);
+
+    calib->t_lin = partial_data2 + (partial_data1 * partial_data1) * calib->par_t3;
+}
+
+float bmp384_compensate_pressure(uint32_t uncomp_press, bmp384_calib_data_t *calib) {
+    float partial_data1 = calib->par_p6 * calib->t_lin;
+    float partial_data2 = calib->par_p7 * (calib->t_lin * calib->t_lin);
+    float partial_data3 = calib->par_p8 * (calib->t_lin * calib->t_lin * calib->t_lin);
+    float partial_out1 = calib->par_p5 + partial_data1 + partial_data2 + partial_data3;
+
+    partial_data1 = calib->par_p2 * calib->t_lin;
+    partial_data2 = calib->par_p3 * (calib->t_lin * calib->t_lin);
+    partial_data3 = calib->par_p4 * (calib->t_lin * calib->t_lin * calib->t_lin);
+    float partial_out2 = (float)uncomp_press * (calib->par_p1 + partial_data1 + partial_data2 + partial_data3);
+
+    partial_data1 = (float)uncomp_press * (float)uncomp_press;
+    partial_data2 = calib->par_p9 + calib->par_p10 * calib->t_lin;
+    partial_data3 = partial_data1 * partial_data2;
+    float partial_data4 = partial_data3 * ((float)uncomp_press * (float)uncomp_press * (float)uncomp_press)*calib->par_p11;
+
+    return partial_out1 + partial_out2 + partial_data4;
+}
 
 void init_spi() {
     spi_bus_config_t buscfg = {
@@ -188,13 +281,42 @@ esp_err_t bmp384_check() {
 
     // Read chip ID
     if (bmp384_read_register(0x00, &chip_id) != ESP_OK || chip_id != 0x50) {
-        ESP_LOGE(TAG, "BMI270 nie wykryty! Chip ID: 0x%02X", chip_id);
+        ESP_LOGE(TAG, "BMP384 nie wykryty! Chip ID: 0x%02X", chip_id);
         return ESP_FAIL;
     }
-    ESP_LOGI(TAG, "BMI270 wykryty. Chip ID: 0x%02X", chip_id);
+    ESP_LOGI(TAG, "BMP384 wykryty. Chip ID: 0x%02X", chip_id);
 
 
     return ESP_OK;
+}
+
+esp_err_t bmp384_read_data()
+{
+    uint8_t raw_data[6];
+    esp_err_t ret = bmp384_read_register_many_bytes(0x04, raw_data, 6);
+
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to read bmp384 data");
+        return ret;
+    }
+
+    // Przyspieszenia
+    uint32_t uncomp_press = (uint32_t)((raw_data[2] << 16) | (raw_data[1] << 8) | (raw_data[0]));
+    uint32_t uncomp_temp = (uint32_t)((raw_data[6] << 16) | (raw_data[5] << 8) | (raw_data[4]));
+
+    bmp384_calib_data_t calib_data;
+
+    bmp384_read_calibration(&calib_data, uncomp_temp);
+    
+    ESP_LOGI(TAG, "Odczytano dane kalibracyjne poprawnie.");
+
+    float comp_press = bmp384_compensate_pressure(uncomp_press, &calib_data);
+
+    ESP_LOGI(TAG, "Cisnienie: = %.2f\n", comp_press);
+
+    return ESP_OK;
+    
+
 }
 
 
@@ -213,5 +335,7 @@ void app_main() {
         return;
     }
     ESP_LOGI(TAG, "Konfiguracja BMP384 przebiegla pomyslnie!");
+
+    bmp384_read_data();
 
 }
